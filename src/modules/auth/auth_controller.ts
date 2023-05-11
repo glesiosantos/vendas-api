03915f4e81bea06ff4c282185ee005a9a82851c3
glesiosantos <glesioss@gmail.com>
2023-05-10 22:05:12 -0300

    criando serviço e rotas para solicitar alteração de e-mail e resetar as senha

--- src/modules/auth/auth_controller.ts
+++ src/modules/auth/auth_controller.ts
@@ -1,6 +1,8 @@
 import { type Request, type Response } from 'express'
 import { AuthRegister } from './services/register_service'
 import { AuthenticationService } from './services/authentication_service'
+import { ResetPasswordService } from '../accounts/services/reset_password_service'
+import { SendForgotPasswordEmailService } from '../accounts/services/send_forgot_password_email_service'
 
 export class AuthController {
   async signin (req: Request, res: Response): Promise<Response> {
@@ -12,6 +14,18 @@ export class AuthController {
   async signup (req: Request, res: Response): Promise<Response> {
     const authService = new AuthRegister()
     await authService.register(req.body)
+    return res.status(204).send()
+  }
+
+  async forgot (req: Request, res: Response): Promise<Response> {
+    const sendForgotService = new SendForgotPasswordEmailService()
+    await sendForgotService.sendEmail(req.body.email)
+    return res.status(204).send()
+  }
+
+  async reset (req: Request, res: Response): Promise<Response> {
+    const resetPasswdService = new ResetPasswordService()
+    await resetPasswdService.resetPassword(req.body)
     return res.status(204).send()
   }
 }
--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,9 +1,22 @@
+import { Joi, Segments, celebrate } from 'celebrate'
 import { type Router } from 'express'
 import { AuthController } from 'src/modules/auth/auth_controller'
+import { JoinAttribute } from 'typeorm/query-builder/JoinAttribute'
 
 const authController = new AuthController()
 
 export default (router: Router): void => {
   router.post('/signin', authController.signin)
   router.post('/signup', authController.signup)
+  router.post('/forgot', celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required()
+    }
+  }), authController.forgot)
+  router.post('/reset', celebrate({
+    [Segments.BODY]: {
+      password: Joi.string().required(),
+      token: Joi.string().required()
+    }
+  }), authController.reset)
 }

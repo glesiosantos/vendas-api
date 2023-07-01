f9d5704a762ef40e04c9f88402ac7a3866e58971
glesiosantos <glesioss@gmail.com>
2023-07-01 04:15:53 -0300

    implementando paginação em customers

--- src/modules/customer/services/list_customer_service.ts
+++ src/modules/customer/services/list_customer_service.ts
@@ -4,7 +4,7 @@ import { Customer } from '../model/customer'
 export class ListCustomerService {
   async load (): Promise<Customer[]> {
     const customerRepository = AppDataSource.getRepository(Customer)
-    const customers = await customerRepository.find()
+    const customers = await customerRepository.createQueryBuilder('customer').take(5).getMany()
     return customers
   }
 }
